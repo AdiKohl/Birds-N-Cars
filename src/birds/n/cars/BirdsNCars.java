d595582be0ce3e830b239595543e1a92ec7d3f5e
AdiKohl <adrian.kohler@stud.hslu.ch>
2017-05-12 12:01:07 +0200

    MainMenueGrundstruktur
    MainMenü grundstgrundstruktur. Es sollte noch die Fenstergrösse und
    hitnergrundbild und buttongrösse angepasst werden.

--- src/birds/n/cars/BirdsNCars.java
+++ src/birds/n/cars/BirdsNCars.java
@@ -5,17 +5,79 @@
  */
 package birds.n.cars;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
 /**
  *
- * @author Adi
+ * @author Team8
  */
-public class BirdsNCars {
 
+public class BirdsNCars extends JFrame implements ActionListener{
+    
+    private Panel panel;
+    private final JButton buttonSingle = new JButton("SINGLEPLAYER");
+    private final JButton buttonMulti = new JButton("MULTIPLAYER");
+    private final JButton buttonOptions = new JButton("OPTIONS");
+    private final JButton buttonExit = new JButton("EXIT");
+    
+    public BirdsNCars() {
+        super("Birds'N'Cars");
+        setDefaultCloseOperation(/*WindowConstants.*/EXIT_ON_CLOSE);
+        setSize(600, 480);
+        setResizable(true);
+        setLayout(new BorderLayout());
+        
+        this.panel = new Panel(new GridLayout(4,1));
+        this.panel.add(this.buttonSingle);
+        this.panel.add(this.buttonMulti);
+        this.panel.add(this.buttonOptions);
+        this.panel.add(this.buttonExit);
+        
+        add(this.panel, BorderLayout.SOUTH);
+        
+        this.buttonSingle.addActionListener(e -> onSingle());
+        this.buttonMulti.addActionListener(e -> onMulti());
+        this.buttonOptions.addActionListener(e -> onOptions());
+        this.buttonExit.addActionListener(e -> onExit());
+        
+        //pack();
+        
+        setVisible(true);
+        
+        
+    }
+    
+    
+    
     /**
      * @param args the command line arguments
      */
+    
     public static void main(String[] args) {
         // TODO code application logic here
+        EventQueue.invokeLater(() -> new BirdsNCars());
     }
+    
+    
+    @Override
+    public void actionPerformed(ActionEvent e){
+        char ch = (e.getActionCommand()).charAt(0);
+    }
+    
+    public void onExit(){
+        System.exit(0);   
+    }
+    
+    public void onOptions(){
+        JOptionPane.showMessageDialog(BirdsNCars.this, "Options not available yet", "Sorry :(",JOptionPane.WARNING_MESSAGE);
+    }
+    public void onSingle(){
+        JOptionPane.showMessageDialog(BirdsNCars.this, "Singleplayer not available yet", "Sorry :(",JOptionPane.WARNING_MESSAGE);
+    }
+    public void onMulti(){
+        JOptionPane.showMessageDialog(BirdsNCars.this, "Multiplayer not available yet", "Sorry :(",JOptionPane.WARNING_MESSAGE);
+    }    
     
 }

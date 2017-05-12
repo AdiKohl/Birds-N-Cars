6d5dcc15ed9011769bbc8122e429243cd8191ac0
LEGhslu <lucien.egloff@stud.hslu.ch>
2017-05-12 12:02:40 +0200

    Figures1

--- src/birds/n/cars/Figures.java
+++ src/birds/n/cars/Figures.java
@@ -11,18 +11,33 @@ package birds.n.cars;
  */
 public class Figures {
     int size;
-    int direction;
     String name;
-    boolean [] hitsArray;
-    int [] location = new int[2];
+    boolean [] hitArray;
+    int [] position = new int[2];
     
     public Figures(int size, int xPosition, int yPosition, int direction, String name){
-            this.hitsArray = new boolean[size];
+            this.hitArray = new boolean[size];
             this.direction = direction;
             this.name = name;
-            this.location [0] = xPosition;
-            this.location [1] = yPosition;
+            this.position [0] = xPosition;
+            this.position [1] = yPosition;
             
+    }
+    
+    public int getSize(){
+        return this.size;
+    }
+    
+    public int getDirection(){
+        return this.direction;
+    };
+    
+    public String getName(){
+        return this.name;
+    }
+    
+    public boolean[] getHitArray(){
+        return this.hitArray;
     }
     
 }

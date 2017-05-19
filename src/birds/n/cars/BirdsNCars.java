068d87ae9388399bb660bb932f32d27624c1a54b
AdiKohl <adrian.kohler@stud.hslu.ch>
2017-05-19 09:40:16 +0200

    Update0939190517

--- src/birds/n/cars/BirdsNCars.java
+++ src/birds/n/cars/BirdsNCars.java
@@ -9,51 +9,19 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
+
 /**
  *
  * @author Team8
  */
 
-public class BirdsNCars extends JFrame implements ActionListener{
-    
-    private Panel panel;
-    private final JButton buttonSingle = new JButton("SINGLEPLAYER");
-    private final JButton buttonMulti = new JButton("MULTIPLAYER");
-    private final JButton buttonOptions = new JButton("OPTIONS");
-    private final JButton buttonExit = new JButton("EXIT");
+public class BirdsNCars {
+   
+   
     
     public BirdsNCars() {
-        super("Birds'N'Cars");
-        
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        
-        
-        
-        setDefaultCloseOperation(/*WindowConstants.*/EXIT_ON_CLOSE);
-        
-        setResizable(true);
-        setLayout(new BorderLayout());
-        
-        this.panel = new Panel(new GridLayout(4,1));
-        this.panel.add(this.buttonSingle);
-        this.panel.add(this.buttonMulti);
-        this.panel.add(this.buttonOptions);
-        this.panel.add(this.buttonExit);
-        
-        add(this.panel, BorderLayout.SOUTH);
-        
-        this.buttonSingle.addActionListener(e -> onSingle());
-        this.buttonMulti.addActionListener(e -> onMulti());
-        this.buttonOptions.addActionListener(e -> onOptions());
-        this.buttonExit.addActionListener(e -> onExit());
-        
-        this.buttonSingle.setPreferredSize(new Dimension(200,200));
-        
-        pack();
-        setBounds(0,0,screenSize.width/3*2, screenSize.height/3*2);
-        
-        setVisible(true);
-        
+        MainMenuWindow menu = new MainMenuWindow();
+        menu.setVisible(true);
         
     }
     
@@ -62,32 +30,12 @@ public class BirdsNCars extends JFrame implements ActionListener{
     /**
      * @param args the command line arguments
      */
-    /*
+    
     public static void main(String[] args) {
         // TODO code application logic here
         EventQueue.invokeLater(() -> new BirdsNCars());
     }
-*/
+
     
-    
-    @Override
-    public void actionPerformed(ActionEvent e){
-        char ch = (e.getActionCommand()).charAt(0);
-    }
-    
-    public void onExit(){
-        System.exit(0);   
-    }
-    
-    public void onOptions(){
-        JOptionPane.showMessageDialog(BirdsNCars.this, "Options not available yet", "Sorry :(",JOptionPane.WARNING_MESSAGE);
-    }
-    public void onSingle(){
-        JOptionPane.showMessageDialog(BirdsNCars.this, "Singleplayer not available yet", "Sorry :(",JOptionPane.WARNING_MESSAGE);
-    }
-    public void onMulti(){
-        //JOptionPane.showMessageDialog(BirdsNCars.this, "Multiplayer not available yet", "Sorry :(",JOptionPane.WARNING_MESSAGE);
-        EventQueue.invokeLater(() -> new MultiplayerMenue());
-    }    
-    
+
 }

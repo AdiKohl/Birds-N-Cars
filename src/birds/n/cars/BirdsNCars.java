9d899ea47cc13a7f4df53b54340369f2a16973f8
AdiKohl <adrian.kohler@stud.hslu.ch>
2017-05-25 18:11:40 +0200

    update1811250517

--- src/birds/n/cars/BirdsNCars.java
+++ src/birds/n/cars/BirdsNCars.java
@@ -15,31 +15,31 @@ import java.awt.event.*;
  * @author Team8
  */
 
-public class BirdsNCars {
+public class BirdsNCars  implements BnCInterface {
    
     ParkingLot parking1;
     ParkingLot parking2;
     MainWindow menu;
    
     
-    public BirdsNCars() {
+    public BirdsNCars(){
         this.menu = new MainWindow();
         menu.setVisible(true);
         
         this.parking1 = new ParkingLot();
         this.parking2 = new ParkingLot();
         
-        parking1.shot(5,5);
-        parking1.placeWreck(1,1);
-        
-        for(int i=0; i<=9; i++){
-            for(int j=0;j<=9;j++){
-                System.out.print(parking1.field[i][j]);                
-            }
-            System.out.println("");
-        }
-        
-        System.out.println("");
+//        parking1.shot(5,5);
+//        parking1.placeWreck(1,1);
+//        
+//        for(int i=0; i<=9; i++){
+//            for(int j=0;j<=9;j++){
+//                System.out.print(parking1.field[i][j]);                
+//            }
+//            System.out.println("");
+//        }
+//        
+//        System.out.println("");
         
 
         
